dc3be8039f7bfb4b3948fa3b1062f5f705a13b83
Simon Desire <85620491+SimonDesire001@users.noreply.github.com>
2023-02-06 21:06:51 +0300

    simoondesire
    simon

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+<<<<<<< Updated upstream
 import 'teethy_exporter.dart';
+=======
+import 'package:flutter/material.dart';
+import 'screens/home/consult.dart';
+>>>>>>> Stashed changes
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
